5b305820e6623e3360e94363e9dbb4aa936f84a0
benpolletta <benpolletta@gmail.com>
2017-05-10 17:00:14 -0400

    find_theta_delta_ratio: finishing script.

--- find_theta_delta_ratio.m
+++ find_theta_delta_ratio.m
@@ -4,6 +4,8 @@ load('subjects.mat'), load('channels.mat')
 
 ch_selected = [1 3];
 
+freq_ranges = {[0.25 4.75], [5.25 10.75]};
+
 present_dir = pwd;
 
 for s = 1:length(subjects)
@@ -18,46 +20,52 @@ for s = 1:length(subjects)
         
         ch_i = ch_selected(ch);
         
-        ch_no = location_channels{s}(ch_i);
+        ch_no = location_channels{ch_i}(s);
         
         epoch_list = sprintf('%s_chan%d_epochs.list', record_dir, ch_no);
         
         epoch_names{ch} = text_read(epoch_list, '%s');
         
-        epoch_no(ch) = length(epoch_names);
+        no_epochs(ch) = length(epoch_names{ch});
         
     end
     
-    if diff(epoch_no) ~= 0
+    if diff(no_epochs) ~= 0
         
-        display(sprintf('For %s, the number of epochs for channel %d and channel %d are different (%d vs. %d).',...
-            subjects{s}, location_channels{s}(ch_selected), epoch_no))
+        fprintf('For %s, the number of epochs for channel %d and channel %d are different (%d vs. %d).\n',...
+            subjects{s}, location_channels{ch_selected(1)}(s), location_channels{ch_selected(2)}(s), no_epochs)
+        
+        cd (present_dir)
        
         return
         
     end
     
-    [power, max] = deal(nan(epoch_no(1), 2));
+    [power, max_freqs, max_vals] = deal(nan(no_epochs(1), 2));
     
-    parfor e = 1:epoch_no(1)
+    parfor e = 1:no_epochs(1)
         
         for ch = 1:2
         
-            data(:, ch) = load(epoch_names{ch}{e});
+            data = load(epoch_names{ch}{e});
+            
+            [~, ~, max_freqs(e, ch), max_vals(e, ch), ~, power(e, ch)] = width_half_max(data, 1000, freq_ranges{ch}, .075, 0)
             
         end
         
-        [whm(e), shm_sum(e), max_freqs(e), max_vals(e), entropy(e)] = width_half_max(data, 1000, [0.25 4.75], .075, 0);
-        
     end
+    
+    val_ratio = max_vals(:, 1)./max_vals(:, 2);
+    
+    pow_ratio = power(:, 1)./power(:, 2);
     
     cd (present_dir)
     
-    save([record_dir, '_chan1_whm.mat'], 'whm', 'shm_sum', 'max_freqs', 'max_vals', 'entropy')
+    save([record_dir, '_tdr.mat'], 'max_freqs', 'max_vals', 'power')
     
-    whm_mat = [whm shm_sum entropy]; no_criteria = size(whm_mat, 2);
+    tdr_mat = [val_ratio pow_ratio power]; no_criteria = size(tdr_mat, 2);
     
-    crit_labels = {'whm', 'shm_sum', 'entropy'};
+    crit_labels = {'val_ratio', 'pow_ratio', 'delta', 'theta'};
     
     figure
     
@@ -65,7 +73,7 @@ for s = 1:length(subjects)
         
         subplot(2, no_criteria, c)
         
-        [n, centers] = hist(whm_mat(:, c), 25);
+        [n, centers] = hist(tdr_mat(:, c), 25);
         
         plot(centers, n)
         
@@ -75,12 +83,12 @@ for s = 1:length(subjects)
         
         subplot(2, no_criteria, no_criteria + c)
         
-        plot(whm_mat(:, c), '*')
+        plot(tdr_mat(:, c), '*')
         
         axis tight
         
     end
     
-    save_as_pdf(gcf, [record_dir, '_chan1_whm'])
+    save_as_pdf(gcf, [record_dir, '_tdr'])
     
 end
