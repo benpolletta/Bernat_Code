9b4a8b33e96e9c700d102d85cb96f7be5ff657c4
benpolletta <benpolletta@gmail.com>
2017-05-25 19:23:26 -0400

    plot_BP_by_entropy: script to plot power by quantile of entropy.

---
+++ plot_BP_by_entropy.m
@@ -0,0 +1,59 @@
+function plot_BP_by_entropy(drug, quantile_used, states)
+
+shm_lim = [1.025 1.25];
+
+load('subjects.mat'), load('AP_freqs.mat')
+
+state_label = ''; long_state_label = '';
+
+if ~isempty(states)
+    
+    no_states = length(states);
+    
+    for s = 1:no_states
+        
+        state_label = [state_label, '_', states{s}];
+        
+        long_state_label = [long_state_label, ', ', states{s}];
+        
+    end
+    
+end
+
+if isscalar(shm_lim)
+    
+    shm_flag = num2str(shm_lim, '%.03f');
+    
+elseif length(shm_lim) == 2
+    
+    shm_flag = sprintf('%.03f_%.03f', shm_lim);
+    
+end
+
+load([drug, '_delta_BP_q', num2str(quantile_used), '_shm', shm_flag, state_label, '_tails.mat'])
+
+entropy_index = 4;
+
+figure
+
+band_labels = {'\delta', '\theta'};
+
+for b = 1:2
+    
+    power = [squeeze(mean_subj_dBP(b, entropy_index, :)) squeeze(mean_subj_ndBP(b, entropy_index, :))];
+    
+    subplot(1, 2, b)
+    
+    barwitherr(nanstd(power), nanmean(power))
+    
+    title(band_labels{b}, 'FontSize', 16)
+    
+    set(gca, 'XTickLabel', {'Narrowband', 'Broadband'})
+    
+    ylabel('%\Delta from Baseline')
+    
+end
+
+mtit(['BP During Narrowband Delta', long_state_label])
+
+save_as_pdf(gcf, [drug, '_delta_BP_q', num2str(quantile_used), state_label, '_power_comparison'])
