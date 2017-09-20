163c1dc0f784ad8293f381b89a9b6d7be56fc058
benpolletta <benpolletta@gmail.com>
2017-09-20 15:00:05 -0400

    NVP_deltaHFO_thetaHFO_plot: added arguments 'drug' and 'hours', to make plot for other drugs and their hours of maximum effect.

--- NVP_deltaHFO_thetaHFO_plot.m
+++ NVP_deltaHFO_thetaHFO_plot.m
@@ -1,25 +1,36 @@
-function NVP_deltaHFO_thetaHFO_plot
+function NVP_deltaHFO_thetaHFO_plot(drug, hour)
+
+if nargin < 2, hour = []; end
+if isempty(hour), hour = 4; end
+hour = sprintf('post%d', hour);
+
+load('drugs.mat')
+
+drug_indicator = strcmp(drugs, drug);
+
+if nargin < 1, drug = ''; end
+if isempty(drug), drug = 'NVP'; end
 
 load('AP_freqs')
 
 figure
 
-%% Plotting Occipital MI for hour 3.
+%% Plotting Occipital MI for hour 4.
             
 load(['ALL_Occipital/ALL_Occipital_p0.99_IEzs_MI/',...
-    'ALL_Occipital_p0.99_IEzs_hrMI_hr_NVP_cplot_data.mat'])
+    'ALL_Occipital_p0.99_IEzs_hrMI_hr_', drug, '_cplot_data.mat'])
 
-p4_indicator = strcmp(cat2_labels, 'post4');
+hour_indicator = strcmp(cat2_labels, hour);
 
 subplot(2, 3, 1)
 
-imagesc(phase_freqs, amp_freqs, MI_stats(:, :, :, p4_indicator, 1))
+imagesc(phase_freqs, amp_freqs, MI_stats(:, :, :, hour_indicator, 1))
 
 axis xy
 
 colorbar
 
-title({'All Occipital PAC'; 'Hr. 4 Post-Injection'; 'Median MI'}, 'FontSize', 16)
+title({'All Occipital PAC'; ['Hr. ', hour(end), ' Post-Injection']; 'Median MI'}, 'FontSize', 16)
 
 ylabel('Amp. Freq. (Hz)', 'FontSize', 14)
 
@@ -35,13 +46,13 @@ for lf = 1:2
     
     subplot(2, 3, 1 + lf)
     
-    imagesc(phase_freqs, amp_freqs, reshape(median_qPAC(:, lf, 3, 2), no_afs, no_pfs))
+    imagesc(phase_freqs, amp_freqs, reshape(median_qPAC(:, lf, drug_indicator, 2), no_afs, no_pfs))
     
     axis xy
     
     colorbar
     
-    title({['Top Quartile for ', HFO_labels{lf}, ' PAC']; 'Hr. 4 Post-Injection'; 'Median MI'}, 'FontSize', 16)
+    title({['Top Quartile for ', HFO_labels{lf}, ' PAC']; ['Hr. ', hour(end), ' Post-Injection']; 'Median MI'}, 'FontSize', 16)
     
     xlabel('Phase Freq. (Hz)', 'FontSize', 14)
     
@@ -60,7 +71,7 @@ p4_index = strcmp(hrs, 'post1') | strcmp(hrs, 'post2') |...
     strcmp(hrs, 'post3') | strcmp(hrs, 'post4');
 states = text_read([ch_dir,'/',ch_dir, '_', measure,'_states.txt'],'%s');
 
-subject = 'A99'; drug = 'NVP'; ds_factor = 22;
+subject = 'A99'; ds_factor = 22;
 
 subj_deltaHFO = nanzscore(sMI(strcmp(subjects, subject) & p4_index & strcmp(drugs, drug), end - 1));
 
@@ -82,7 +93,7 @@ h = subplot(2, 2, 3);
 
 hrs = 4*(1:length(subj_deltaHFO))/length(subj_deltaHFO);
     
-set(h, 'NextPlot', 'add', 'ColorOrder', [0 0 1; 1 0 0])
+set(h, 'NextPlot', 'add', 'ColorOrder', [0 1 0; 1 0 0])
 
 plot(hrs', [subj_deltaHFO subj_thetaHFO], 'LineWidth', 2)
 
@@ -106,7 +117,7 @@ xlabel('Time (h) Rel. Injection', 'FontSize', 14)
 
 %% Plotting correlation of delta-HFO and theta-HFO PAC.
 
-load(['Occipital_NVP_thetaHFO_deltaHFO_multidrug_ds', num2str(ds_factor, '%.3g'), '.mat'])
+load(['Occipital_', drug, '_thetaHFO_deltaHFO_multidrug_ds', num2str(ds_factor, '%.3g'), '.mat'])
 
 subplot(2, 3, 6)
 
@@ -124,4 +135,4 @@ plot(c{1}, p_fit(1)*c{1} + p_fit(2), 'w')
 
 title({'Correlation of Post-Injection \delta-HFO and \theta-HFO PAC'; ['\rho = ', num2str(rho, '%.3g'), ', p = ', num2str(p, '%.3g')]}, 'FontSize', 16)
 
-save_as_pdf(gcf, 'NVP_dHFO_tHFO_figure')
+save_as_pdf(gcf, [drug, '_dHFO_tHFO_figure'])
