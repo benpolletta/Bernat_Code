ed7562aeacd0029d1fcb1afc5352f91954e98ead
benpolletta <benpolletta@gmail.com>
2017-09-20 15:11:50 -0400

    multidrug_plot_delta_MI: changing subplot locations to preserve similar aspect ratio in figure.

--- multidrug_plot_delta_MI.m
+++ multidrug_plot_delta_MI.m
@@ -4,6 +4,8 @@ load('AP_freqs')
 
 load('drugs')
 
+load('subjects')
+
 state_label = '';
 
 if ~isempty(states)
@@ -28,7 +30,8 @@ if ~isempty(states)
     
 end
 
-suffix = ['_delta_MI_q', num2str(quantile_used), '_shm', sprintf('%.03f_%.03f', shm_lims), state_label, '_tails'];
+
+suffix = ['_delta_MI_q', num2str(quantile_used), make_label('shm', shm_lims, []), state_label, '_tails'];
 
 figure
 
@@ -44,25 +47,33 @@ for d = 1:no_drugs
     
     cmax = max(max(median_dMI(:, entropy_index)), max(median_ndMI(:, entropy_index)));
     
-    subplot(no_drugs, 2, 2*(d - 1) + 1)
+    subplot(no_drugs, 3, 3*(d - 1) + 1)
     
     imagesc(phase_freqs, amp_freqs, reshape(median_dMI(:, entropy_index), no_afs, no_pfs))
+    
+    set(gca, 'FontSize', 16)
     
     axis xy
     
     caxis([cmin cmax])
     
-    ylabel(drugs{d})
+    ylabel({drugs{d}; 'Amp. Freq. (Hz)'})
     
     if d == 1
     
         title({'Narrowband Delta'; long_state_label}, 'FontSize', 16)
         
+    elseif d == 4
+        
+        xlabel('Phase Freq. (Hz)')
+        
     end
     
-    subplot(no_drugs, 2, 2*(d - 1) + 2)
+    subplot(no_drugs, 3, 3*(d - 1) + 2)
     
     imagesc(phase_freqs, amp_freqs, reshape(median_ndMI(:, entropy_index), no_afs, no_pfs))
+    
+    set(gca, 'FontSize', 16)
     
     axis xy
     
@@ -71,7 +82,31 @@ for d = 1:no_drugs
     if d == 1
     
         title({'Broadband Delta'; long_state_label}, 'FontSize', 16)
+        
+    elseif d == 4
+        
+        xlabel('Phase Freq. (Hz)')
     
+    end
+    
+end
+
+shm_label = make_label('shm', [1.025 1.25], []);
+
+load('drugs.mat'), load('subjects.mat'), load('AP_freqs.mat')
+
+state_label = ''; long_state_label = '';
+
+if ~isempty(states)
+    
+    no_states = length(states);
+    
+    for s = 1:no_states
+        
+        state_label = [state_label, '_', states{s}];
+        
+        long_state_label = [long_state_label, ', ', states{s}];
+        
     end
     
 end
